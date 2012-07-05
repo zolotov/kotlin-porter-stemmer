0ec54078457d8f80a776b8cda26551ca12729e4c
Alexander Zolotov <Alexander.Zolotov@jetbrains.com>
2012-07-06 01:51:16 +0400

    - add Stemmer method stubs - add tests for step1-methods - add tests for m-method - implement step1a

--- src/main/kotlin/com/alexzolotov/nlp/Stemmer.kt
+++ src/main/kotlin/com/alexzolotov/nlp/Stemmer.kt
@@ -2,8 +2,53 @@ package com.alexzolotov.nlp
 
 public class Stemmer {
 
-    public fun stem(word : String): String {
+    public fun stem(var word : String): String {
+        word = step1(word)
+        word = step2(word)
+        word = step3(word)
+        word = step4(word)
+        return step5(word)
+    }
+
+    fun step1(word : String) : String {
+        return step1b(step1a(word));
+    }
+
+    fun step2(var word : String) : String {
         return word
     }
 
+    fun step3(var word : String) : String {
+        return word
+    }
+
+    fun step4(var word : String) : String {
+        return word
+    }
+
+    fun step5(var word : String) : String {
+        return word
+    }
+
+    fun step1a(word : String) : String {
+        return when {
+            word.endsWith("sses") -> word.replaceFirst("sses$", "ss")
+            word.endsWith("ies") -> word.substring(0, word.length - 2)
+            word.endsWith("ss") -> word
+            word.endsWith("s") -> word.substring(0, word.length - 1)
+            else -> word
+        }
+    }
+
+    fun step1b(word : String) : String {
+        return word
+    }
+
+    fun step1c(word : String) : String {
+        return word
+    }
+
+    fun m(word : String) : Int {
+        return 0
+    }
 }
---
+++ src/test/kotlin/com/alexzolotov/nlp/StemmerTest.kt
@@ -0,0 +1,81 @@
+package com.alexzolotov.nlp
+
+import kotlin.test.assertEquals
+import org.testng.annotations.Test as test
+
+class StemmerTest {
+    val stemmer = Stemmer()
+
+    test fun step1a() {
+        assertEquals("caress", stemmer.step1a("caresses"))
+        assertEquals("poni", stemmer.step1a("ponies"))
+        assertEquals("ti", stemmer.step1a("ties"))
+        assertEquals("caress", stemmer.step1a("caress"))
+        assertEquals("cat", stemmer.step1a("cats"))
+    }
+
+    test fun step1b() {
+        assertEquals("agree", stemmer.step1b("agreed"))
+        assertEquals("plaster", stemmer.step1b("plastered"))
+        assertEquals("bled", stemmer.step1b("bled"))
+        assertEquals("motor", stemmer.step1b("motoring"))
+        assertEquals("sing", stemmer.step1b("sing"))
+
+        assertEquals("conflate", stemmer.step1b("conflated"))
+        assertEquals("trouble", stemmer.step1b("troubled"))
+        assertEquals("size", stemmer.step1b("sized"))
+        assertEquals("hop", stemmer.step1b("hopping"))
+        assertEquals("tan", stemmer.step1b("tanned"))
+        assertEquals("hiss", stemmer.step1b("hissing"))
+        assertEquals("fizz", stemmer.step1b("fizzed"))
+        assertEquals("fail", stemmer.step1b("failed"))
+        assertEquals("file", stemmer.step1b("filling"))
+    }
+
+    test fun step1c() {
+        assertEquals("happy", stemmer.step1c("happy"))
+        assertEquals("ski", stemmer.step1c("sky"))
+    }
+
+    test fun step1() {
+        assertEquals("caress", stemmer.step1("caresses"))
+        assertEquals("poni", stemmer.step1("ponies"))
+        assertEquals("ti", stemmer.step1("ties"))
+        assertEquals("caress", stemmer.step1("caress"))
+        assertEquals("cat", stemmer.step1("cats"))
+        assertEquals("agree", stemmer.step1("agreed"))
+        assertEquals("plaster", stemmer.step1("plastered"))
+        assertEquals("bled", stemmer.step1("bled"))
+        assertEquals("motor", stemmer.step1("motoring"))
+        assertEquals("sing", stemmer.step1("sing"))
+        assertEquals("conflate", stemmer.step1("conflated"))
+        assertEquals("trouble", stemmer.step1("troubled"))
+        assertEquals("size", stemmer.step1("sized"))
+        assertEquals("hop", stemmer.step1("hopping"))
+        assertEquals("tan", stemmer.step1("tanned"))
+        assertEquals("hiss", stemmer.step1("hissing"))
+        assertEquals("fizz", stemmer.step1("fizzed"))
+        assertEquals("fail", stemmer.step1("failed"))
+        assertEquals("file", stemmer.step1("filling"))
+        assertEquals("happy", stemmer.step1("happy"))
+        assertEquals("ski", stemmer.step1("sky"))
+    }
+
+    test fun m() {
+        assertEquals(0, stemmer.m("tr"))
+        assertEquals(0, stemmer.m("ee"))
+        assertEquals(0, stemmer.m("tree"))
+        assertEquals(0, stemmer.m("y"))
+        assertEquals(0, stemmer.m("by"))
+
+        assertEquals(1, stemmer.m("trouble"))
+        assertEquals(1, stemmer.m("oats"))
+        assertEquals(1, stemmer.m("trees"))
+        assertEquals(1, stemmer.m("ivy"))
+
+        assertEquals(2, stemmer.m("troubles"))
+        assertEquals(2, stemmer.m("private"))
+        assertEquals(2, stemmer.m("oaten"))
+        assertEquals(2, stemmer.m("orrery"))
+    }
+}
